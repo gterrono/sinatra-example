71f544efe439464135213b33967a0277e10eb4a6
Greg Terrono <gterrono@seas.upenn.edu>
2012-09-03 05:25:24 -0400

    Added links to the models

--- models.rb
+++ models.rb
@@ -1,5 +1,15 @@
-require 'data_mapper'
+require 'dm-core'
+require 'dm-timestamps'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db.db")
+
+class Link
+  include DataMapper::Resource
+  property :id, Serial
+  property :url, String, :length => 200
+  property :keyword, String
+  property :created_on, Date
+  property :hits, Integer :default => 0
+end
 
 DataMapper.auto_migrate!
