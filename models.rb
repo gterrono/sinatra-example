176072aa63667d0df1d6455d04f6ea857c55a2e4
Greg Terrono <gterrono@seas.upenn.edu>
2012-09-06 02:25:39 -0400

    Fixed up my models

--- models.rb
+++ models.rb
@@ -1,5 +1,7 @@
 require 'dm-core'
+require 'dm-migrations'
 require 'dm-timestamps'
+require 'dm-validations'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db.db")
 
@@ -7,9 +9,12 @@ class Link
   include DataMapper::Resource
   property :id, Serial
   property :url, String, :length => 200
-  property :keyword, String
+  property :keyword, String, :unique => true,
+    :messages => {
+      :is_unique => 'A URL with that keyword already exists. Please enter a new one, or use a default.'
+    }
   property :created_on, Date
-  property :hits, Integer :default => 0
+  property :hits, Integer, :default => 0
 end
 
 DataMapper.auto_migrate!
