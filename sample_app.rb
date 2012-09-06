bb93f43640c571434572421c837608a0fc0de50c
Greg Terrono <gterrono@seas.upenn.edu>
2012-09-06 02:26:37 -0400

    Added error checking for duplication of keywords

--- sample_app.rb
+++ sample_app.rb
@@ -4,3 +4,14 @@ require_relative 'models'
 get '/' do
   erb :index
 end
+
+post '/' do
+  link = Link.create(params)
+  if link.saved?
+    'Your link was saved'
+  else
+    @entered_data = params
+    @error = link.errors.first[0]
+    erb :index
+  end
+end
